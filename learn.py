b3835c01dc81a7d9eb13317aca3f64bc23640970
Jason Baker <amnorvend@gmail.com>
2015-05-29 17:37:51 -0700

    Traverse tree and store it in sqlite.

--- learn.py
+++ learn.py
@@ -14,19 +14,40 @@ def cluster(distances):
     for j, name_j in enumerate(names):
       mat[i][j] = distances.get((name_i, name_j)) or distances.get((name_j, name_i)) or 0
 
-  global linkage_matrix, T
-
   condensed = distance.squareform(mat)
   linkage_matrix = hierarchy.complete(condensed)
-  clusters = hierarchy.fcluster(linkage_matrix, criterion='maxclust', t=5)
-  zipped_clusters = zip(names, clusters)
+  leaves_dict = {}
+  traverse_tree(hierarchy.to_tree(linkage_matrix), leaves_dict)
+  print(leaves_dict)
+
   with contextlib.closing(sqlite3.connect('voynich.db')) as connection:
     cursor = connection.cursor()
-    connection.execute('CREATE TABLE IF NOT EXISTS clusters (name, clusterid)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS clusters(name, clusterid)')
     cursor.execute('DELETE FROM clusters')
-    for name, clusterid in zipped_clusters:
-      cursor.execute('INSERT INTO clusters(name, clusterid) values (\'%s\', %d)' % (name, clusterid))
+    for key, values in leaves_dict.items():
+      for clusterid in values:
+        cursor.execute('INSERT INTO clusters(name, clusterid) VALUES (?, ?)', 
+          (names[key], clusterid))
     connection.commit()
 
-  hierarchy.dendrogram(linkage_matrix, labels=names)
-  matplotlib.pyplot.show()
+
+  # T = clusters = hierarchy.fcluster(linkage_matrix, criterion='maxclust', t=5)
+  # zipped_clusters = zip(names, clusters)
+  # with contextlib.closing(sqlite3.connect('voynich.db')) as connection:
+  #   cursor = connection.cursor()
+  #   connection.execute('CREATE TABLE IF NOT EXISTS clusters (name, clusterid)')
+  #   cursor.execute('DELETE FROM clusters')
+  #   for name, clusterid in zipped_clusters:
+  #     cursor.execute('INSERT INTO clusters(name, clusterid) values (\'%s\', %d)' % (name, clusterid))
+  #   connection.commit()
+
+  # hierarchy.dendrogram(linkage_matrix, labels=names)
+  # matplotlib.pyplot.show()
+
+def traverse_tree(tree, leaves_dict, cluster_list=()):
+  if tree.is_leaf():
+    leaves_dict[tree.get_id()] = cluster_list
+  else:
+    cluster_list = cluster_list + (tree.get_id(),)
+    traverse_tree(tree.get_left(), leaves_dict, cluster_list)
+    traverse_tree(tree.get_right(), leaves_dict, cluster_list)
